0d0dd680a46639fdff7b0359902a763d3ecdc16a
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-01-16 00:13:53 +0530

    Updated home

--- resources/js/Home/Home.jsx
+++ resources/js/Home/Home.jsx
@@ -48,18 +48,18 @@ class Home extends Component {
                 </div>
                 <div className="col-sm-12">
                   <h3>What Is TEDxKCCEMSR?</h3>
-                  <span>TEDxKCCEMSR is an independently organised TED- like event by K.C College Of Engineering And Management studies And Research. The scope of the event is to provide a platform where community-driven people can showcase their ideas and elevating them to a global stage. </span>
+                  <span>TEDxKCCEMSR is an independently organised TEDx event by K.C College Of Engineering And Management studies And Research. The scope of the event is to provide a platform where community-driven people can showcase their ideas and elevating them to a global stage. </span>
                 </div>
                 <div className="col-sm-12">
                   <div className="theme center">
                     <h3>Theme</h3>
                     <img src="/img/theme.png"/>
                   </div>
-                  <p>With the advent of modern technology, human sciences and habitat, the challenges we face have also changed. Thus, the traditional ways of tackling them fail. Here arises the need of redefining perspectives. </p>
+                  <p>With the advent of modern technology, human sciences and habitat, the challenges we face have also changed. Thus, the traditional ways of tackling them fail. Here arises the need of redefining perspectives.</p>
                   <p>Perspectives inspire decisions and your decisions fuel your actions.</p>
                   <p>Similarly, each individual has his or her own perspective when placed in common circumstance. However, the most unorthodox, original, economically and environmentally profiting perspective thrives the longest in a race.</p>
                   <p>The famous Penrose triangle is recognized as "Impossibility in it's purest form". At first glance, it's a three shade triangle with no possible story to tell, but on further inspection the complexity of the Penrose triangle from every angle puts our thought into a paradoxical perspective of everything we see.</p>
-                  <p>We believe that making the impossible, possible is no lesser than an evolution of progress.  All you need is a redefined perspective to look at problem, adapt a bright idea and live through it.</p>
+                  <p>We believe that making the impossible, possible is no lesser than an evolution of progress. All you need is a redefined perspective to look at problem, adapt a bright idea and live through it.</p>
                 </div>
               </div>
             </div>
