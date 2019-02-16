5b40457711f98cdb04db54fe4f68138737d87ec4
Bhaktij Koli <bhaktijkoli121@gmail.com>
2019-02-16 22:07:24 +0530

    Removed countdown

--- resources/js/Home/CountDown.jsx
+++ resources/js/Home/CountDown.jsx
@@ -8,20 +8,12 @@ class CountDown extends Component {
     super(props);
   }
   componentDidMount() {
-    var date1 = new Date(2019,1,16);
-    var date2 = new Date();
-
-    var secs = Math.abs((date1.getTime() - date2.getTime())/ 1000);
-    var twoDaysFromNow = (new Date().getTime() / 1000) + secs + 1;
-
-    new FlipDown(twoDaysFromNow, 'countdown', {theme: 'light'}).start();
   }
   render() {
     return (
       <div className="container">
         <div className="center">
           <h1><span>Redefining</span> Perspective</h1>
-          <div id="countdown" className="flipdown"></div>
           <span>A platform to bring leading thinkers and unheard voices, to share ideas worth spreading to local communities.</span>
         </div>
       </div>
